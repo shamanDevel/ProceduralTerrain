0fbb203992397e68f9fec9f9771f05db53c97388
shamanDevel <muell.filter@gmail.com>
2015-11-16 22:56:59 -0500

    voronoi-noise added and perlin-noise improved

--- src/org/shaman/terrain/polygonal/GraphToHeightmap.java
+++ src/org/shaman/terrain/polygonal/GraphToHeightmap.java
@@ -10,6 +10,7 @@ import com.jme3.app.Application;
 import com.jme3.material.Material;
 import com.jme3.material.RenderState;
 import com.jme3.math.ColorRGBA;
+import com.jme3.math.FastMath;
 import com.jme3.math.Vector3f;
 import com.jme3.renderer.Camera;
 import com.jme3.renderer.ViewPort;
@@ -56,26 +57,26 @@ public class GraphToHeightmap {
 	private static final Map<Biome, double[]> BIOME_PROPERTIES = new HashMap<>();
 	static {
 		BIOME_PROPERTIES.put(Biome.SNOW, new double[]{0.5, 0.7, 0.7, 0.5});
-		BIOME_PROPERTIES.put(Biome.TUNDRA, new double[]{0.5, 0.5, 0.3, 0.5});
-		BIOME_PROPERTIES.put(Biome.BARE, new double[]{0.4, 0.4, 0.3, 0.5});
+		BIOME_PROPERTIES.put(Biome.TUNDRA, new double[]{0.5, 0.5, 0.5, 0.5});
+		BIOME_PROPERTIES.put(Biome.BARE, new double[]{0.4, 0.4, 0.5, 0.5});
 		BIOME_PROPERTIES.put(Biome.SCORCHED, new double[]{0.7, 0.3, 0, 0});
 		BIOME_PROPERTIES.put(Biome.TAIGA, new double[]{0.4, 0.3, 0, 0});
 		BIOME_PROPERTIES.put(Biome.SHRUBLAND, new double[]{0.5, 0.2, 0, 0});
-		BIOME_PROPERTIES.put(Biome.TEMPERATE_DESERT, new double[]{0.1, 0.1, 0.4, 0});
+		BIOME_PROPERTIES.put(Biome.TEMPERATE_DESERT, new double[]{0.1, 0.1, 0.3, 0});
 		BIOME_PROPERTIES.put(Biome.TEMPERATE_RAIN_FOREST, new double[]{0.3, 0.2, 0, 0});
 		BIOME_PROPERTIES.put(Biome.TEMPERATE_DECIDUOUS_FOREST, new double[]{0.3, 0.4, 0, 0});
 		BIOME_PROPERTIES.put(Biome.GRASSLAND, new double[]{0.4, 0.5, 0, 0});
-		BIOME_PROPERTIES.put(Biome.TROPICAL_RAIN_FOREST, new double[]{0.3, 0.2, 0.1, 0});
+		BIOME_PROPERTIES.put(Biome.TROPICAL_RAIN_FOREST, new double[]{0.3, 0.2, 0.05, 0});
 		BIOME_PROPERTIES.put(Biome.TROPICAL_SEASONAL_FOREST, new double[]{0.3, 0.2, 0, 0});
-		BIOME_PROPERTIES.put(Biome.SUBTROPICAL_DESERT, new double[]{0.3, 0.6, 0.5, 0});
-		BIOME_PROPERTIES.put(Biome.BEACH, new double[]{0.3, 0.5, 0.4, 0});
-		BIOME_PROPERTIES.put(Biome.LAKE, new double[]{0.2, 0.2, 0.2, 0});
+		BIOME_PROPERTIES.put(Biome.SUBTROPICAL_DESERT, new double[]{0.3, 0.6, 0.3, 0});
+		BIOME_PROPERTIES.put(Biome.BEACH, new double[]{0.3, 0.5, 0.1, 0});
+		BIOME_PROPERTIES.put(Biome.LAKE, new double[]{0.2, 0.2, 0, 0});
 		BIOME_PROPERTIES.put(Biome.OCEAN, new double[]{0.2, 0.1, 0, 0});
 	}
 	private static final int NOISE_OCTAVES = 6;
 	private static final float NOISE_OCTAVE_FACTOR = 2;
 	private static final float BASE_FREQUENCY = 1/16f;
-	private static final float PERLIN_NOISE_SCALE = 0.1f;
+	private static final float PERLIN_NOISE_SCALE = 0.2f;
 	private static final float DISTORTION_FREQUENCY = 32;
 	private static final float DISTORTION_AMPLITUDE = 0.01f;
 	private static final float SMOOTHING_STEPS = 10;
@@ -84,6 +85,11 @@ public class GraphToHeightmap {
 		{-1, -1}, {-1, 0}, {-1, 1},
 		{0, 1}, {0, -1}
 	};
+	private static final int VORONOI_CELL_COUNT = 32;
+	private static final int VORONOI_POINTS_PER_CELL = 3;
+	private static final float VORONOI_DISTORTION_FREQUENCY = 32;
+	private static final float VORONOI_DISTORTION_AMPLITUDE = 0.01f;
+	private static final float VORONOI_SCALE = 1f;
 	
 	//Input
 	private final Graph graph;
@@ -128,7 +134,152 @@ public class GraphToHeightmap {
 	}
 	
 	private void calculateElevation() {
-		//BASE ELEVATION
+		calculateBaseElevation();
+		
+		//get noise parameters
+		Geometry geom = createNoiseGeometry();
+		noise = new float[size][size][3];
+		renderColor(noise, geom, ColorRGBA.Black, 0, 1);
+		LOG.info("noise properties calculated");
+		
+		addPerlinNoise();
+		
+		addVoronoiNoise();
+	}
+
+	private void addPerlinNoise() {
+		float[] perlinFactors = new float[NOISE_OCTAVES];
+		Noise[] noiseGenerators = new Noise[NOISE_OCTAVES];
+		for (int i=0; i<NOISE_OCTAVES; ++i) {
+			noiseGenerators[i] = new Noise(rand.nextLong());
+			perlinFactors[i] = (float) (BASE_FREQUENCY * Math.pow(NOISE_OCTAVE_FACTOR, i));
+		}
+		Heightmap values = new Heightmap(size);
+		float min = Float.POSITIVE_INFINITY;
+		float max = Float.NEGATIVE_INFINITY;
+		for (int x=0; x<size; ++x) {
+			for (int y=0; y<size; ++y) {
+				float roughness = noise[x][y][1];
+				//multi-fractal perlin noise
+				double perlin = 0;
+				for (int i=0; i<NOISE_OCTAVES; ++i) {
+					perlin += noiseGenerators[i].noise(perlinFactors[i]*x, perlinFactors[i]*y)
+							/ Math.pow(NOISE_OCTAVE_FACTOR, i*(1-roughness));
+				}
+				values.setHeightAt(x, y, (float) perlin);
+				min = (float) Math.min(perlin, min);
+				max = (float) Math.max(perlin, max);
+			}
+		}
+		float factor = 1f / (max-min);
+		for (int x=0; x<size; ++x) {
+			for (int y=0; y<size; ++y) {
+				float amplitude = noise[x][y][0];
+				float perlin = (values.getHeightAt(x, y) - min) * factor;
+				perlin *= amplitude * PERLIN_NOISE_SCALE;
+				heightmap.adjustHeightAt(x, y, perlin);
+			}
+		}
+		LOG.info("perlin noise added");
+	}
+	
+	private void addVoronoiNoise() {
+		float cellSize = (float) size / (float) VORONOI_CELL_COUNT;
+		List<Vector3f> pointList = new ArrayList<>();
+		for (int x=0; x<VORONOI_CELL_COUNT; ++x) {
+			for (int y=0; y<VORONOI_CELL_COUNT; ++y) {
+				float nx = x*cellSize;
+				float ny = y*cellSize;
+				for (int i=0; i<VORONOI_POINTS_PER_CELL; ++i) {
+					pointList.add(new Vector3f(
+							nx + rand.nextFloat()*cellSize,
+							ny + rand.nextFloat()*cellSize,
+							rand.nextFloat()));
+				}
+			}
+		}
+		Vector3f[] points = pointList.toArray(new Vector3f[pointList.size()]);
+		LOG.info("voronoi point created");
+		//now cycle through cells and find influencing hills
+		Heightmap tmp = new Heightmap(size);
+		Vector3f first = new Vector3f();
+		Vector3f second = new Vector3f();
+		float min = Float.POSITIVE_INFINITY;
+		float max = Float.NEGATIVE_INFINITY;
+		for (int x=0; x<size; ++x) {
+			for (int y=0; y<size; ++y) {
+				//sort points by distance to (x,y)
+				final float px = x;
+				final float py = y;
+				findClosestTwoPoints(points, px, py, first, second);
+				assert (dist(first, px, py) <= dist(second, px, py));
+				//calc height
+				float v = -1*dist(first, px, py) 
+						+ 1*dist(second, px, py);
+				tmp.setHeightAt(x, y, v);
+				min = Math.min(v, min);
+				max = Math.max(v, max);
+			}
+		}
+		//normalize
+		float factor = 1f / (max-min);
+		for (int x=0; x<size; ++x) {
+			for (int y=0; y<size; ++y) {
+				float h = tmp.getHeightAt(x, y);
+				tmp.setHeightAt(x, y, (h-min) * factor);
+			}
+		}
+		LOG.info("voronoi cells calculated");
+		//distort
+		Noise distortionNoise = new Noise(rand.nextLong());
+		Heightmap tmp2 = new Heightmap(size);
+		for (int x=0; x<size; ++x) {
+			for (int y=0; y<size; ++y) {
+				float s = x/(float)size;
+				float t = y/(float)size;
+				float ss = (float) (s + VORONOI_DISTORTION_AMPLITUDE 
+						* 2*distortionNoise.noise(s*VORONOI_DISTORTION_FREQUENCY, 
+								t*VORONOI_DISTORTION_FREQUENCY, 0));
+				float tt = (float) (t + VORONOI_DISTORTION_AMPLITUDE 
+						* 2*distortionNoise.noise(s*VORONOI_DISTORTION_FREQUENCY, 
+								t*VORONOI_DISTORTION_FREQUENCY, 3.4));
+				float v = tmp.getHeightInterpolating(ss*size, tt*size);
+				tmp2.setHeightAt(x, y, v);
+			}
+		}
+		LOG.info("voronoi cells distorted");
+		//apply
+		for (int x=0; x<size; ++x) {
+			for (int y=0; y<size; ++y) {
+				float amplitude = noise[x][y][2];
+				heightmap.adjustHeightAt(x, y, amplitude * tmp.getHeightAt(x, y) * VORONOI_SCALE);
+			}
+		}
+		
+		LOG.info("voronoi noise added");
+	}
+	private static void findClosestTwoPoints(Vector3f[] points, float px, float py, Vector3f first, Vector3f second) {
+		float dist1 = Float.POSITIVE_INFINITY;
+		float dist2 = Float.POSITIVE_INFINITY;
+		for (Vector3f p : points) {
+			float d = dist(p, px, py);
+			if (d<dist1) {
+				dist2 = dist1;
+				second.set(first);
+				dist1 = d;
+				first.set(p);
+			} else if (d<dist2) {
+				dist2 = d;
+				second.set(p);
+			}
+		}
+	}
+	private static float dist(Vector3f hillCenter, float px, float py) {
+		//return FastMath.sqrt((hillCenter.x-px)*(hillCenter.x-px) + (hillCenter.y-py)*(hillCenter.y-py))*hillCenter.z;
+		return ((hillCenter.x-px)*(hillCenter.x-px) + (hillCenter.y-py)*(hillCenter.y-py))*hillCenter.z;
+	}
+
+	private void calculateBaseElevation() {
 		//assign elevation to oceans
 		for (Graph.Corner c : graph.corners) {
 			if (c.ocean) {
@@ -156,6 +307,14 @@ public class GraphToHeightmap {
 		Geometry geom = createElevationGeometry();
 		Heightmap tmp = new Heightmap(size);
 		render(tmp.getRawData(), geom, ColorRGBA.Black, -1, 1);
+		//scale
+		for (int x=0; x<size; ++x) {
+			for (int y=0; y<size; ++y) {
+				float h = tmp.getHeightAt(x, y);
+				h = Math.signum(h) * h * h;
+				tmp.setHeightAt(x, y, h);
+			}
+		}
 		//distort
 		Noise distortionNoise = new Noise(rand.nextLong());
 		for (int x=0; x<size; ++x) {
@@ -180,35 +339,6 @@ public class GraphToHeightmap {
 		}
 		assignCenterElevations();
 		LOG.info("base elevation assigned");
-		
-		//NOISE
-		//get noise parameters
-		geom = createNoiseGeometry();
-		noise = new float[size][size][3];
-		renderColor(noise, geom, ColorRGBA.Black, 0, 1);
-		LOG.info("noise properties calculated");
-		//init noise
-		float[] perlinFactors = new float[NOISE_OCTAVES];
-		Noise[] noiseGenerators = new Noise[NOISE_OCTAVES];
-		for (int i=0; i<NOISE_OCTAVES; ++i) {
-			noiseGenerators[i] = new Noise(rand.nextLong());
-			perlinFactors[i] = (float) (BASE_FREQUENCY * Math.pow(NOISE_OCTAVE_FACTOR, i));
-		}
-		for (int x=0; x<size; ++x) {
-			for (int y=0; y<size; ++y) {
-				float amplitude = noise[x][y][0];
-				float roughness = noise[x][y][1];
-				float voronoiAmpl = noise[x][y][2];
-				//multi-fractal perlin noise
-				double perlin = 0;
-				for (int i=0; i<NOISE_OCTAVES; ++i) {
-					perlin += noiseGenerators[i].noise(perlinFactors[i]*x, perlinFactors[i]*y)
-							/ Math.pow(NOISE_OCTAVE_FACTOR, i*(1-roughness));
-				}
-				perlin *= amplitude * PERLIN_NOISE_SCALE;
-				heightmap.adjustHeightAt(x, y, (float) perlin);
-			}
-		}
 	}
 	private void smooth_old(Heightmap map) {
 		Heightmap m = map.clone();
@@ -325,7 +455,11 @@ public class GraphToHeightmap {
 		ArrayList<ColorRGBA> colorList = new ArrayList<>(graph.centers.size());
 		for (Graph.Center c : graph.centers) {
 			double[] settings = BIOME_PROPERTIES.get(c.biome);
-			ColorRGBA col = new ColorRGBA((float) settings[0], (float) settings[1], (float) (settings[2] + settings[3]*c.elevation), 1);
+			float r = (float) settings[0];
+			float g = (float) settings[1];
+			float b = (float) (settings[2] + settings[3]*Math.max(0, c.elevation)*Math.max(0, c.elevation));
+			//float b = Math.max(0, c.elevation); b*=b;
+			ColorRGBA col = new ColorRGBA(r, g, b, 1);
 			colorList.add(col);
 			posList.add(new Vector3f(c.location.x, c.location.y, 0));
 		}
