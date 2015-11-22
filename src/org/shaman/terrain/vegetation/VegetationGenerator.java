ecc7414be67c6f42f55fae47c41ad2fb86a6a929
shamanDevel <muell.filter@gmail.com>
2015-11-21 21:30:16 -0500

    started with ui interaction

--- src/org/shaman/terrain/vegetation/VegetationGenerator.java
+++ src/org/shaman/terrain/vegetation/VegetationGenerator.java
@@ -9,10 +9,12 @@ import com.jme3.math.Vector3f;
 import de.lessvoid.nifty.Nifty;
 import java.util.List;
 import java.util.logging.Logger;
+import javax.annotation.Nullable;
 import org.shaman.terrain.AbstractTerrainStep;
 import org.shaman.terrain.Heightmap;
 import org.shaman.terrain.erosion.RiverSource;
 import org.shaman.terrain.erosion.WaterErosionScreenController;
+import org.shaman.terrain.polygonal.Biome;
 
 /**
  *
@@ -23,6 +25,11 @@ public class VegetationGenerator extends AbstractTerrainStep {
 	
 	//GUI and settings
 	private VegetationScreenController screenController;
+	private boolean editDensity = false;
+	private Biome selectedBiome;
+	private float brushSize;
+	private float plantSize;
+	private long seed;
 	
 	//Input
 	private Vector3f originalMapScale;
@@ -72,4 +79,36 @@ public class VegetationGenerator extends AbstractTerrainStep {
 		
 	}
 	
+	void guiBiomeSelected(@Nullable Biome biome) {
+		LOG.info("biome selected: "+biome);
+		selectedBiome = biome;
+	}
+	void guiBrushSizeChanged(float size) {
+		LOG.info("brush size changed: "+size);
+		brushSize = size;
+	}
+	void guiSeedChanged(long seed) {
+		LOG.info("seed changed: "+seed);
+		this.seed = seed;
+	}
+	void guiEditDensity(boolean editing) {
+		LOG.info("density editing "+(editing ? "enabled" : "disabled"));
+		editDensity = editing;
+	}
+	void guiPlantSizeChanged(float scale) {
+		LOG.info("plant size changed: "+scale);
+		plantSize = scale;
+	}
+	void guiGeneratePlants() {
+		LOG.info("generate plants");
+		//Test
+		screenController.setGenerating(true);
+		screenController.setProgress(0.5f);
+	}
+	void guiCancelGeneration() {
+		LOG.info("cancel generating");
+		//Test
+		screenController.setGenerating(false);
+		screenController.setProgress(1);
+	}
 }
--- src/org/shaman/terrain/vegetation/VegetationScreenController.java
+++ src/org/shaman/terrain/vegetation/VegetationScreenController.java
@@ -7,11 +7,12 @@ package org.shaman.terrain.vegetation;
 
 import de.lessvoid.nifty.Nifty;
 import de.lessvoid.nifty.NiftyEventSubscriber;
-import de.lessvoid.nifty.controls.ButtonClickedEvent;
-import de.lessvoid.nifty.controls.CheckBoxStateChangedEvent;
-import de.lessvoid.nifty.controls.SliderChangedEvent;
+import de.lessvoid.nifty.controls.*;
+import de.lessvoid.nifty.elements.Element;
+import de.lessvoid.nifty.examples.progressbar.ProgressbarControl;
 import de.lessvoid.nifty.screen.Screen;
 import de.lessvoid.nifty.screen.ScreenController;
+import java.util.Random;
 
 /**
  *
@@ -19,6 +20,20 @@ import de.lessvoid.nifty.screen.ScreenController;
  */
 public class VegetationScreenController implements ScreenController {
 	private final VegetationGenerator generator;
+	private final Random rand = new Random();
+	
+	private Nifty nifty;
+	private Screen screen;
+	private Slider brushSizeSlider;
+	private Element biomeImage;
+	private Label selectedBiomeLabel;
+	private TextField seedTextField;
+	private Button newSeedButton;
+	private CheckBox densityCheckBox;
+	private Slider plantSizeSlider;
+	private Button generatePlantsButton;
+	private Button cancelGenerationButton;
+	private ProgressbarControl generatePlantsProgressbar;
 
 	public VegetationScreenController(VegetationGenerator generator) {
 		this.generator = generator;
@@ -26,12 +41,29 @@ public class VegetationScreenController implements ScreenController {
 
 	@Override
 	public void bind(Nifty nifty, Screen screen) {
-		
+		this.nifty = nifty;
+		this.screen = screen;
 	}
 
 	@Override
 	public void onStartScreen() {
+		brushSizeSlider = screen.findNiftyControl("BrushSizeSlider", Slider.class);
+		biomeImage = screen.findElementById("BiomeImage");
+		selectedBiomeLabel = screen.findNiftyControl("SelectedBiomeLabel", Label.class);
+		seedTextField = screen.findNiftyControl("SeedTextField", TextField.class);
+		newSeedButton = screen.findNiftyControl("SeedButton", Button.class);
+		densityCheckBox = screen.findNiftyControl("DensityCheckBox", CheckBox.class);
+		plantSizeSlider = screen.findNiftyControl("PlantSizeSlider", Slider.class);
+		generatePlantsButton = screen.findNiftyControl("GeneratePlantsButton", Button.class);
+		cancelGenerationButton = screen.findNiftyControl("CancelGenerationButton", Button.class);
+		generatePlantsProgressbar = screen.findControl("GeneratePlantsProgressbar", ProgressbarControl.class);
 		
+		cancelGenerationButton.setEnabled(false);
+		generator.guiBrushSizeChanged(brushSizeSlider.getValue());
+		generator.guiPlantSizeChanged(plantSizeSlider.getValue());
+		String seed = randomSeed();
+		seedTextField.setText(seed);
+		generator.guiSeedChanged(seed.hashCode());
 	}
 
 	@Override
@@ -39,26 +71,69 @@ public class VegetationScreenController implements ScreenController {
 		
 	}
 	
+	private String randomSeed() {
+		return Long.toHexString(Math.abs(rand.nextLong() % (1l<<48)));
+	}
+	
+	void setGenerating(boolean generating) {
+		brushSizeSlider.setEnabled(!generating);
+		selectedBiomeLabel.setEnabled(!generating);
+		seedTextField.setEnabled(!generating);
+		newSeedButton.setEnabled(!generating);
+		densityCheckBox.setEnabled(!generating);
+		plantSizeSlider.setEnabled(!generating);
+		generatePlantsButton.setEnabled(!generating);
+		cancelGenerationButton.setEnabled(generating);
+	}
+	void setProgress(float progress) {
+		generatePlantsProgressbar.setProgress(progress);
+	}
+	
 	@NiftyEventSubscriber(pattern = ".*Button")
 	public void onButtonClick(String id, ButtonClickedEvent e) {
-		System.out.println("button "+id+" clicked: "+e);
-		
+		System.out.println("button "+id+" clicked");
+		if (newSeedButton == e.getButton()) {
+			String seed = randomSeed();
+			seedTextField.setText(seed);
+			generator.guiSeedChanged(seed.hashCode());
+		} else if (generatePlantsButton == e.getButton()) {
+			generator.guiGeneratePlants();
+		} else if (cancelGenerationButton == e.getButton()) {
+			generator.guiCancelGeneration();
+		}
+	}
+	
+	@NiftyEventSubscriber(pattern = ".*TextField")
+	public void onTextfieldChange(String id, TextFieldChangedEvent e) {
+		System.out.println("textfield "+id+" changed: "+e.getText());
+		if (seedTextField == e.getTextFieldControl()) {
+			generator.guiSeedChanged(e.getText().hashCode());
+		}
 	}
 	
 	@NiftyEventSubscriber(pattern = ".*CheckBox")
 	public void onCheckBoxClick(String id, CheckBoxStateChangedEvent e) {
-		System.out.println("checkbox "+id+" changed: "+e);
-		
+		System.out.println("checkbox "+id+" changed: "+e.isChecked());
+		if (densityCheckBox == e.getCheckBox()) {
+			generator.guiEditDensity(e.isChecked());
+		}
 	}
 	
 	@NiftyEventSubscriber(pattern = ".*Slider")
 	public void onSliderChange(String id, SliderChangedEvent e) {
-		System.out.println("slider "+id+" changed: "+e);
-		
+		System.out.println("slider "+id+" changed: "+e.getValue());
+		if (brushSizeSlider == e.getSlider()) {
+			generator.guiBrushSizeChanged(e.getValue());
+		} else if (plantSizeSlider == e.getSlider()) {
+			generator.guiPlantSizeChanged(e.getValue());
+		}
 	}
 	
 	public void biomeSelected(int x, int y) {
-		
+		x -= biomeImage.getX();
+		y -= biomeImage.getY();
+		System.out.println("biome clicked at "+x+":"+y);
+		//TODO
 	}
 	
 }
